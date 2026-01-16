65d9cf09d7b56bcfaa3d805f1216091a2261e6ad
Parbhat Sharma <prabhat29ps@gmail.com>
2026-01-16 11:58:56 +0530

    nginx api gateway with auth

---
+++ auth/src/auth.ts
@@ -0,0 +1,36 @@
+import type {Request, Response} from 'express';
+import {verify} from 'jsonwebtoken';
+
+export const authenticateUser = async(req: Request, res: Response) => {
+    try {
+        const authHeader = req.headers['authorization'];
+
+        if (!authHeader) {
+            return res.status(401).json({message: 'Authorization header missing'});
+        }
+
+        const token = authHeader.split(' ')[1];
+
+        if (!token) {
+            return res.status(401).json({message: 'Token missing'});
+        }
+
+        const jwtSecret = process.env.JWT_SECRET;
+
+        if (!jwtSecret) {
+            return res.status(500).json({message: 'JWT secret not configured'});
+        }
+
+        const payload = verify(token, jwtSecret);
+
+        console.log(payload);
+
+        res.setHeader('X-User-Id', (payload as any).sub);
+        res.setHeader('X-User-Email', (payload as any).email);
+        res.setHeader('X-User-Name', (payload as any).user_metadata?.full_name || '');
+
+        return res.status(200).json({message: 'User authenticated'});
+    } catch (error) {
+        return res.status(401).json({message: 'Invalid token'});
+    }
+}
---
+++ auth/src/index.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import {config} from 'dotenv';
+
+import { authenticateUser } from './auth';
+
+config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.get('/validate_token', authenticateUser);
+
+app.listen(port, () => {
+  console.log(`Auth service listening at http://localhost:${port}`);
+});
+export default app;
