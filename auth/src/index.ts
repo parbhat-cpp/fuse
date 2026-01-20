b665419c7af41d546c4ca1388885cb360c5770a0
Parbhat Sharma <prabhat29ps@gmail.com>
2026-01-20 12:05:46 +0530

    auth: handle all methods

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -11,7 +11,7 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-app.get('/validate_token', authenticateUser);
+app.all('/validate_token', authenticateUser);
 
 app.listen(port, () => {
   console.log(`Auth service listening at http://localhost:${port}`);
