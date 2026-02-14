4c90e79b2c80aa1e276cd443c888a5930a266566
Parbhat Sharma <prabhat29ps@gmail.com>
2026-02-14 16:44:48 +0530

    workers: add notifications worker

---
+++ workers/notifications/inapp/src/index.ts
@@ -0,0 +1,47 @@
+import { Worker } from 'bullmq';
+import { Redis } from 'ioredis';
+
+// redis connection
+const redisConn = new Redis({
+  host: process.env.REDIS_HOST!,
+  port: parseInt(process.env.REDIS_PORT!, 10) || 6379,
+  password: process.env.REDIS_PASSWORD!,
+  maxRetriesPerRequest: null,
+});
+
+// define worker
+const worker = new Worker(
+  'in-app-notification',
+    async (job) => {
+        const response = await fetch(`http://notifications-dev:8000/save`,{
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(job.data),
+        });
+
+        if (!response.ok) {
+          throw new Error(`Failed to save notification: ${response.statusText}`);
+        }
+    },
+    {
+        connection: redisConn,
+    },
+);
+
+worker.on('completed', (job) => {
+    console.log(`Job ${job.id} completed`);
+});
+
+worker.on('failed', (job, err) => {
+    console.error(`Job ${job?.id} failed`, err);
+});
+
+worker.on('error', (err) => {
+    console.error('Worker error', err);
+});
+
+worker.on('drained', () => {
+    console.log('Queue drained');
+});
