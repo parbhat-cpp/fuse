fd309fb285fe98be0ab4faf3831698c964d73b29
Parbhat Sharma <prabhat29ps@gmail.com>
2026-04-09 09:17:31 +0530

    frontend: enable sitemap

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,25 +1,33 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
 
-const config = defineConfig({
-  plugins: [
-    // this is the plugin that enables path aliases
-    viteTsConfigPaths({
-      projects: ['./tsconfig.json'],
-    }),
-    tailwindcss(),
-    tanstackStart({
-      customViteReactPlugin: true,
-      prerender: {
-        enabled: true,
-        crawlLinks: true,
-      }
-    }),
-    viteReact(),
-  ],
+const config = defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [
+      // this is the plugin that enables path aliases
+      viteTsConfigPaths({
+        projects: ['./tsconfig.json'],
+      }),
+      tailwindcss(),
+      tanstackStart({
+        customViteReactPlugin: true,
+        prerender: {
+          enabled: true,
+          crawlLinks: true,
+        },
+        sitemap: {
+          enabled: true,
+          host: env.VITE_HOST || 'http://localhost:5173',
+        },
+      }),
+      viteReact(),
+    ],
+  };
 })
 
 export default config
